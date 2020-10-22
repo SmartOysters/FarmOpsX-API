5bd8f4232afda1dc1db03043ddca2cfa13105125
James Rickard <james@frodosghost.com>
2020-10-22 13:48:27 +1100

    Resource endpoints added to the system

--- src/Resources/Reports.php
+++ src/Resources/Reports.php
@@ -16,5 +16,94 @@ use SmartOysters\FarmOpsX\Response;
 
 class Reports extends Resource
 {
-    //
+    protected $disabled = ['list', 'fetch', 'create', 'update'];
+
+    /**
+     * Return a Report
+     *
+     * @param int $reportId
+     * @param string|null $stateId
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function get(int $reportId, $stateId = null)
+    {
+        return $this->request->get(':reportId', compact('reportId', 'stateId'));
+    }
+
+    /**
+     * Get Reports by SaferMe ChannelID
+     *
+     * @param int $channelId
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function channel(int $channelId)
+    {
+        return $this->request->get('channel/:channelId', compact('channelId'));
+    }
+
+    /**
+     * Get Reports by SaferMe ChannelID
+     *
+     * @param string $id
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function report($id)
+    {
+        return $this->request->get('entry/:id', compact('id'));
+    }
+
+    /**
+     * Remove report entry
+     *
+     * @param string $id
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function delete($id)
+    {
+        return $this->request->delete('entry/:id', compact('id'));
+    }
+
+    /**
+     * Get Reports by SaferMe TeamID
+     *
+     * @param int $teamId
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function team($teamId)
+    {
+        return $this->request->get('team/:teamId', compact('teamId'));
+    }
+
+    /**
+     * Run the Exporter
+     *
+     * @param array $teamIds
+     * @param string|\DateTime|null $to
+     * @param string|\DateTime|null $from
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function export($teamIds = [], $to = null, $from = null)
+    {
+        if ($to instanceof \DateTime) {
+            $to = $to->format('Y-m-d');
+        }
+        if ($from instanceof \DateTime) {
+            $from = $from->format('Y-m-d');
+        }
+
+        return $this->request->post('export', compact('teamIds'));
+    }
+
+    /**
+     * Run the Importer
+     *
+     * @param $teamId
+     * @param array $chanelIds
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function import($teamId, $chanelIds = [])
+    {
+        return $this->request->post('import', compact('teamId', 'chanelIds'));
+    }
 }
+
