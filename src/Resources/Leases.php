8129989e9a49c77e42633a7e05be4b2a788ba826
James Rickard <james@frodosghost.com>
2020-10-22 12:53:19 +1100

    Updated Leases with correct endpoints.

--- src/Resources/Leases.php
+++ src/Resources/Leases.php
@@ -16,5 +16,40 @@ use SmartOysters\FarmOpsX\Response;
 
 class Leases extends Resource
 {
-    //
+    protected $disabled = ['list', 'fetch', 'create', 'update'];
+
+    /**
+     * Returns all leases in the system
+     *
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function all()
+    {
+        return $this->request->get('');
+    }
+
+    /**
+     * Get a specific Lease
+     *
+     * @param int $leaseId
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function get(int $leaseId)
+    {
+        return $this->request->get(':leaseId', compact('leaseId'));
+    }
+
+    /**
+     * Add a capacity to a Lease
+     *
+     * @param int $leaseId
+     * @param int $capacity
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function capacity(int $leaseId, int $capacity)
+    {
+        $data["$leaseId"] = $capacity;
+
+        return $this->request->put('capacity', $data);
+    }
 }
