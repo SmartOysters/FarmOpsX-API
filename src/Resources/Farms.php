131bf4564f73db3108982fd8b0a1e7dfd3501f6e
James Rickard <james@frodosghost.com>
2021-06-02 23:15:15 +1000

    Changes to allow add and edit.

--- src/Resources/Farms.php
+++ src/Resources/Farms.php
@@ -16,5 +16,30 @@ use SmartOysters\FarmOpsX\Response;
 
 class Farms extends Resource
 {
-    //
+    protected $disabled = ['fetch', 'create', 'update'];
+
+    /**
+     * Add Farm data to the API
+     *
+     * @param array $values
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function add($values = [])
+    {
+        return $this->request->post('', $values);
+    }
+
+    /**
+     * Edit Farm information
+     *
+     * @param int   $farmId
+     * @param array $values
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function edit($farmId, $values = [])
+    {
+        return $this->request->put(':farmId', array_merge([
+            'farmId' => $farmId
+        ], $values));
+    }
 }
