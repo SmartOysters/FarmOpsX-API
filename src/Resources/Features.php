248f3e5e0c7dcc3d81f3da49f5bd066216c60a02
Liam Claridge <liam@smartoysters.com>
2022-12-02 10:36:03 +1100

    Create Features class is markDeleted function.

---
+++ src/Resources/Features.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the FarmOpsX API PHP Package
+ *
+ * (c) James Rickard <james.rickard@smartoysters.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SmartOysters\FarmOpsX\Resources;
+
+use SmartOysters\FarmOpsX\Resources\Base\Resource;
+use SmartOysters\FarmOpsX\Response;
+
+class Features extends Resource
+{
+    protected $disabled = ['list', 'fetch', 'create', 'update', 'delete'];
+
+    /**
+     * Mark the Shape as deleted
+     *
+     * @param int $featureId
+     * @return \SmartOysters\FarmOpsX\Http\Response
+     */
+    public function markDeleted($featureId)
+    {
+        return $this->request->delete(':featureId', compact('featureId'));
+    }
+}
